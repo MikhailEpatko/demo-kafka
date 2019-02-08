3d9648573da1c6bf0b5b2ef4bb4cfaa9d9f1e448
Mikhail Epatko <mikhail.epatko@paragon-software.com>
2019-02-08 16:47:26 +0300

    Init project

---
+++ src/main/java/ru/epatko/demorabbitkafka/DemoRabbitKafkaApplication.java
@@ -0,0 +1,14 @@
+package ru.epatko.demorabbitkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoRabbitKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoRabbitKafkaApplication.class, args);
+	}
+
+}
+
